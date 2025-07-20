063dcc94d798dc788d5bad786bdafb81b1edc761
Gavin-Roche <cubergr@gmail.com>
2025-07-20 15:43:30 +0100

    Added Hangman_ascii_drawing.cpp

---
+++ Minimal_concept_test_programs/Hangman_ascii_drawing.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // this file makes a hangman out of ascii characters
+    cout << "     +---+" << endl << "     |   |" << endl << "     O   |" << endl << "    /|\\\  |" << endl << "    / \\\  |" << endl << "         |" << endl << "    ======";
+}
